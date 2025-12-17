139987bc0129a88ba6da532bfc69bc38c6fd908d
Prateek Kumar Lohani <151424017+lohaniprateek@users.noreply.github.com>
2025-12-17 17:09:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  plugins: [react()],
   base: '/arpitdhoundiyal08/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
